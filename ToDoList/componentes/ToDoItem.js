1b9c98f8ad27c28fb13049f10bc89404edcfbd49
LucasTimoteo <95625902+LucasDoGit@users.noreply.github.com>
2023-08-22 12:19:31 -0300

    Incluído animação na criação da tefas
    Criado animação ao clicar em uma tarefa, ela expande e abre a descrição. Também foi adicionado um datapicker.

--- ToDoList/componentes/ToDoItem.js
+++ ToDoList/componentes/ToDoItem.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Switch } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Switch, LayoutAnimation, UIManager, Platform } from 'react-native';
+
+if(Platform.OS === 'android') {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 const TodoItem = ({ item, trocaEstado, deleta }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const expanded = () => {
+        LayoutAnimation.spring()
+        setIsExpanded(!isExpanded)
+    }
+
     return (
-      <View style={styles.todoItem}>
-        <Switch 
-            value={item.completado}
-            onValueChange={() => trocaEstado(item.id)}    
-        />
-        <Text style={item.completado ? styles.completedText : styles.text}>
-            {item.text}
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.todoItem}>
+            <Switch 
+                value={item.completado}
+                onValueChange={() => trocaEstado(item.id)}    
+            />
+        <TouchableOpacity onPress={expanded}>
+            <Text style={item.completado ? styles.completedText : styles.text}>
+                {item.text.nome}
+            </Text>
+        </TouchableOpacity>
         <TouchableOpacity onPress={() => deleta(item.id)}>
             <Text style={styles.deleteButton}>Excluir</Text>
         </TouchableOpacity>
-      </View>  
+        </View>
+        {isExpanded &&  (
+            <View>
+                <Text style={styles.text}><Text style={{fontWeight: 'bold'}}>&#8226; Data: </Text>{item.tarefa.data.toLocaleString().split(' ')[0]}</Text>
+                <Text style={styles.text}><Text style={{fontWeight: 'bold'}}>&#8226; Descrição: </Text>{item.tarefa.descricao}</Text>
+            </View>      
+        )}
+      </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        backgroundColor: '#ededed',
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+    },
     todoItem: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingVertical: 10,
         paddingHorizontal: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
+        backgroundColor: '#fff'
     },
     text: {
         fontSize: 18,
--- ToDoList/telas/addTarefas.js
+++ ToDoList/telas/addTarefas.js
@@ -1,25 +1,55 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text, Platform } from 'react-native';
 import Header from '../componentes/Header';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const TelaAddTarefa = ({ navigation, route }) => {
-    const [text, setText] = useState('');
+    const [tarefa, setTarefa] = useState({nome: '', descricao: '', data: new Date(Date.now())});
+    const [showDataPicker, setShowDataPicker] = useState(false);
 
     const adicionaTarefa = () => { 
-        route.params.addTarefa(text);
-        setText('');
+        route.params.addTarefa(tarefa);
+        setTarefa({nome: '', descricao: '', data: new Date(Date.now())});
         navigation.goBack();
     };
+
+    const saveDate = (event, value) => {
+        setTarefa({...tarefa, data: value});
+        if (Platform.OS === 'android') {
+            setShowDataPicker(false);
+        }
+    }
 
     return (
         <View style={styles.container}>
             <Header />
             <TextInput 
                 style={styles.input}
-                placeholder="Nova tarefa"
-                value={text}
-                onChangeText={(value) => setText(value)}
+                placeholder="Nome da Tarefa"
+                value={tarefa.nome}
+                onChangeText={(value) => setTarefa({...tarefa, nome: value})}
             />
+            <TextInput 
+                style={[styles.input, styles.descricao]}
+                placeholder="Descrição"
+                multiline ={true}
+                value={tarefa.descricao}
+                onChangeText={(value) => setTarefa({...tarefa, descricao: value})}
+            />
+                        {!showDataPicker && (
+                <View style={styles.dateButton}>
+                <Button title={tarefa.data.toLocaleString().split(' ')[0]} 
+                onPress={() => setShowDataPicker(true)} />
+                </View>
+            )}
+            {showDataPicker && (
+                <DateTimePicker 
+                value={tarefa.data}
+                display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                onChange={saveDate}
+                style={styles.datePicker}
+                />
+            )}
             <Button title="Salvar" onPress={adicionaTarefa} />
         </View>
     );
@@ -30,13 +60,30 @@ const styles = StyleSheet.create ({
         flex: 1,
         backgroundColor: '#fff'
     },
+    descricao: {
+        height: 120
+    },
     input: {
         height: 40,
         borderColor: '#ccc',
         borderWidth: 1, 
         marginBottom: 10,
-        paddingHorizontal: 10,
+        marginTop: 10,
+        padding: 10,
     },
+    dateButton: {
+        //flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+    datePicker: {
+        width: 320,
+        height: 260,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    }
 });
 
 export default TelaAddTarefa;
